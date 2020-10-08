22b862c9c513a659fc96bc54d6edb9433c44e7ff
elTorado <45456651+elTorado@users.noreply.github.com>
2020-10-08 16:46:14 +0200

    Add files via upload

--- SoftCon2020_Assignment_2/src/NamedRow.java
+++ SoftCon2020_Assignment_2/src/NamedRow.java
@@ -5,25 +5,31 @@ import java.util.List;
 public  class NamedRow implements Row {
     public int rowNumber=0;
 
-    public List<String> rowList = Arrays.asList("[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]");
+    public String stringRepr = " [] [] [] [] [] [] [] [] [] [] ";
+
+    // public List<String> rowList = Arrays.asList("[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]");
+
+    public char[] listRepr = stringRepr.toCharArray();
 
     public  int getRowNumber(){
         return rowNumber;
     }
 
-    public  List<String> getrowList(){
-        return rowList;
-    };
+    public String getStringRepr(){
+        return stringRepr;
+    }
+
+    public char[] getListRepr(){return  listRepr;}
 
     public  void setRowNumber(int number){
         this.rowNumber = number;
-    };
+    }
 
     // replaces element in rowlist at input place with intput letter
-    public void setRowInstance(int place, String letter){
-        this.rowList.set(place, letter);
+    public void setRowInstance(int place, char letter){
+        this.listRepr[place] = letter;
     };
 
-    public void printRow(){System.out.println(Arrays.toString((rowList.toArray())));}
+    public void printRow(){System.out.println(stringRepr);}
 
 }
--- SoftCon2020_Assignment_2/src/Row.java
+++ SoftCon2020_Assignment_2/src/Row.java
@@ -5,11 +5,13 @@ public interface Row {
 
     public int getRowNumber();
 
-    public  List<String> getrowList();
+    public String getStringRepr();
+
+    public char[] getListRepr();
 
     public  void setRowNumber(int number);
 
     // replaces element in rowlist at input place with intput letter
-    public  void setRowInstance(int place, String letter);
+    public  void setRowInstance(int place, char letter);
 }
 
