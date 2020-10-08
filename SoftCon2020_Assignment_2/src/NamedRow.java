f678af72406d410b4a4f465cfdc72e52b6970ba5
elTorado <45456651+elTorado@users.noreply.github.com>
2020-10-08 16:18:27 +0200

    Add files via upload

--- SoftCon2020_Assignment_2/src/NamedRow.java
+++ SoftCon2020_Assignment_2/src/NamedRow.java
@@ -1,2 +1,29 @@
-public class NamedRow {
+import java.util.Arrays;
+import java.util.List;
+
+
+public  class NamedRow implements Row {
+    public int rowNumber=0;
+
+    public List<String> rowList = Arrays.asList("[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]");
+
+    public  int getRowNumber(){
+        return rowNumber;
+    }
+
+    public  List<String> getrowList(){
+        return rowList;
+    };
+
+    public  void setRowNumber(int number){
+        this.rowNumber = number;
+    };
+
+    // replaces element in rowlist at input place with intput letter
+    public void setRowInstance(int place, String letter){
+        this.rowList.set(place, letter);
+    };
+
+    public void printRow(){System.out.println(Arrays.toString((rowList.toArray())));}
+
 }
--- SoftCon2020_Assignment_2/src/Row.java
+++ SoftCon2020_Assignment_2/src/Row.java
@@ -1,2 +1,15 @@
-public class Row {
+
+import java.util.*;
+
+public interface Row {
+
+    public int getRowNumber();
+
+    public  List<String> getrowList();
+
+    public  void setRowNumber(int number);
+
+    // replaces element in rowlist at input place with intput letter
+    public  void setRowInstance(int place, String letter);
 }
+
