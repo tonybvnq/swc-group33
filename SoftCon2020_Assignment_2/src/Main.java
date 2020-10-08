6b33a729c393345ec9d03b0e3bf36469d4aeacc0
eoeaee <47557704+eoeaee@users.noreply.github.com>
2020-10-08 15:02:15 +0200

    Added new folder for assignment 2

---
+++ SoftCon2020_Assignment_2/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Battleship!!");
+
+        //**Initialize**
+        //Create and maintain rows with Lists
+            //Get status of specific row
+            //Set ship in specific row
+
+        //Create Ships
+            //Get length of ship
+            //Get letter of ship
+            //Get amount of ship type
+            //Get list of all ships
+
+
+        //**User Input**
+        //x10
+        //Ask User for placement of each ship
+            //Check if inside of board
+
+        //Check ship placement
+            //Check length of ship
+            //Check status of specific rows
+
+        //Place ship in rows(s)
+            //Set letter of ship in row(s))
+
+
+        //**Command line output**
+        //Assemble rows and lists and print filled board
+
+    }
+}
+
