dfafe8c0adec6589ec242cff1c79a5e18b772367
eoeaee <47557704+eoeaee@users.noreply.github.com>
2020-10-14 14:23:54 +0200

    Added new classes and instructions

--- SoftCon2020_Assignment_2/src/InputGetter.java
+++ SoftCon2020_Assignment_2/src/InputGetter.java
@@ -1,16 +1,19 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
-/*
-calls positioning class
+import java.util.Arrays;
+//calls positioning class
 
 public class InputGetter {
-    public void askPlacement() {
+    public static String[] askPlacement() {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Please enter the position of your :");
 
         String returnedShipPlacement = myObj.nextLine();  // Read user input
 
-        //Test comment
+        String[] listOfPlaces = returnedShipPlacement.split(" ");
+        System.out.println(Arrays.toString(listOfPlaces));
+
+        return listOfPlaces;
     }
 
 }
-*/
--- SoftCon2020_Assignment_2/src/Main.java
+++ SoftCon2020_Assignment_2/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Battleship!!");
 
+        //Test Input getter
+        InputGetter.askPlacement();
 
         //**Initialize**
         //Create and maintain rows with Lists
