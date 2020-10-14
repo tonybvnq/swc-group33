4a440e4b3beaf65565013f232d97968901268e22
eoeaee <47557704+eoeaee@users.noreply.github.com>
2020-10-14 15:18:55 +0200

    Added new classes and instructions

--- SoftCon2020_Assignment_2/src/InputGetter.java
+++ SoftCon2020_Assignment_2/src/InputGetter.java
@@ -1,7 +1,7 @@
 import java.lang.reflect.Array;
 import java.util.Scanner;
 import java.util.Arrays;
-//calls positioning class
+
 
 public class InputGetter {
     public static String[] askPlacement(String shipName) {
