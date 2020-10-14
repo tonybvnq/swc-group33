4e101498a99a41b5c4a7b15cc039eacc17576581
eoeaee <47557704+eoeaee@users.noreply.github.com>
2020-10-14 17:28:47 +0200

    Added new classes and instructions

--- SoftCon2020_Assignment_2/src/InputGetter.java
+++ SoftCon2020_Assignment_2/src/InputGetter.java
@@ -19,22 +19,39 @@ public class InputGetter {
             String secondEntry = listOfPlaces[1];
             String ycoordinates = "ABCDEFGHIL";
             String xcoordinates = "0123456789";
+            char firstEntryx = firstEntry.charAt(0);
+            char secondEntryx = secondEntry.charAt(0);
+            char firstEntryy = firstEntry.charAt(1);
+            char secondEntryy = secondEntry.charAt(1);
+            int numE1x = firstEntryx;
+            int numE2x = secondEntryx;
+            int numE1y = firstEntryy;
+            int numE2y = secondEntryy;
 
             //Test whether out of board
-            if (ycoordinates.indexOf(firstEntry.charAt(0))!=-1 && ycoordinates.indexOf(secondEntry.charAt(0))!=-1 && xcoordinates.indexOf(firstEntry.charAt(1))!=-1 && xcoordinates.indexOf(secondEntry.charAt(1))!=-1) {
+            if (ycoordinates.indexOf(firstEntryx) != -1
+                    && ycoordinates.indexOf(secondEntryx) != -1
+                    && xcoordinates.indexOf(firstEntryy) != -1
+                    && xcoordinates.indexOf(secondEntryy) != -1) {
+                inputIsValid = true;
+            }
+            if (!inputIsValid) {
+                System.out.println("Chosen coordinates are outside of board!!! Please try again");
+            }
+            //Test whether length is correct
+            int length = ship.length;
+
+            if ((firstEntryx == secondEntryx && (numE2y - numE1y) == length)
+                    || (firstEntryy == secondEntryy && (numE2x - numE1x) == length)) {
                 inputIsValid = true;
             } else {
                 inputIsValid = false;
             }
-
-            //Test whether length is correct
-
+            if (!inputIsValid) {
+                System.out.println("Coordinates do not fit length of ship!!! Please try again");
+            }
 
         }
-
-        //Test
-        System.out.println(Arrays.toString(listOfPlaces));
-
         return listOfPlaces;
     }
 
