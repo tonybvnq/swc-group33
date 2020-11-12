ed6119f0ffdba540bb0d7f2b8f16530e7b473d56
elTorado <45456651+elTorado@users.noreply.github.com>
2020-11-12 09:59:22 +0100

    Update Customer.java

--- SoftCon2020_Assignment_4/src/Customer.java
+++ SoftCon2020_Assignment_4/src/Customer.java
@@ -1,22 +1,77 @@
 public class Customer {
     // name
     private String name;
+
     // surname
     private String surname;
+
     // age
     private int age;
+
     // bank account number
     private int accountNumber;
+
     // savings
     private SavingsAccount savingsAccount;
+
+    private CreditCard creditCard;
+
     // unique Identification Number (ID)
     private int UID;
 
-    private enum customerLevel{
+    public enum customerLevel{
         Regular,
         Gold,
         Platinum
     }
+
+    private customerLevel currentLevel;
+
+
+    // SETTERS AND GETTERS
+    public void setName(String name) { this.name = name; }
+    public  void setSurname(String surname) {this.surname = surname; }
+    public void setAge(int age) {this.age = age; }
+    public void setAccountNumber(int accountNumber) { this.accountNumber = accountNumber; }
+    public void setSavingsAccount(SavingsAccount savingsAccount) { this.savingsAccount = savingsAccount; }
+    public void setcustomerLevel(customerLevel customerlevel){this.currentLevel = customerlevel; }
+    public void setUID(int UID) {this.UID = UID; }
+
+
+    public CreditCard getCreditCard(){ return this.creditCard;}
+    public String getFullName(){ return this.surname + " " + this.name; }
+    public int getAge(){return this.age;}
+    public int getAccountNumber(){return this.accountNumber;}
+    public SavingsAccount getSavingsAccount(){return this.savingsAccount;}
+    public int getUID(){return this.UID;}
+    public customerLevel getCurrentLevel(){
+        return this.currentLevel;
+    }
+    public void setCreditCard(){
+            if(this.getCurrentLevel() == customerLevel.Regular ){
+                this.creditCard = new RegularCard();
+            }
+            else if (this.getCurrentLevel() == customerLevel.Gold ){
+                this.creditCard = new GoldCard();
+            }
+            else {this.creditCard = new PlatinumCard();}
+    }
+
+
+    // INITIALIZATION
+    public Customer(String name, String surname, int age, int accountNumber, int UID){
+        this.setName(name);
+        this.setSurname(surname);
+        this.setAge(age);
+        this.setAccountNumber(accountNumber);
+
+        // CREATE SAVINGS ACCOUNT OR SO??
+
+        this.setUID(UID);
+        this.setcustomerLevel(customerLevel.Regular);
+        this.setCreditCard();
+    }
+
 
     // Level
 
@@ -32,5 +87,29 @@ public class Customer {
     // 10000 CHF by credit card per transaction. Payments with bank transfer do
     // not have an amount limit.
 
+    public void deposit(int money){
+        this.getSavingsAccount().addMoney(money);
+    }
+
+    public int withdraw(int money){
+        if (money > this.getSavingsAccount().getAmount()){
+            System.out.println("NOT ENOUGH MONEY ON ACCOUNT");
+            return 0;}
+        else(this.getSavingsAccount().removeMoney(money));
+        return money;
+    }
+
+    void payFromAccount(int money){
+        if (money > this.getSavingsAccount().getAmount()){
+        System.out.println("NOT ENOUGH MONEY ON ACCOUNT");}
+        else(this.getSavingsAccount().removeMoney(money));
+    }
+
+    void payWithCard(int money){
+        if (money > this.getCreditCard().getCardLimit){
+            System.out.println("CARDLIMIT IS TO LOW TO PAY THIS PRICE");
+        }
+        else(this.getCreditCard().remove(money));
+    }
 
 }
