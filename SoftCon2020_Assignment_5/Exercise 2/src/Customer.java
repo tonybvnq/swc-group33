29c437f77f13d334d8cbbe449827bf4822448b68
Tony Bang <71351936+tonybvnq@users.noreply.github.com>
2020-11-19 16:08:41 +0100

    Update Customer.java

--- SoftCon2020_Assignment_5/Exercise 2/src/Customer.java
+++ SoftCon2020_Assignment_5/Exercise 2/src/Customer.java
@@ -1,3 +1,6 @@
+/**
+ * Customers have a specific car type and a method to print the car type's name, space, speed and price
+ */
 public class Customer {
     private String cartype;
     public String getCartype() {return cartype; }
