eb66141375165a5bf1c333fed8f831adb3a4dd9f
Tony Bang <71351936+tonybvnq@users.noreply.github.com>
2020-11-19 16:09:43 +0100

    Update Customer.java

--- SoftCon2020_Assignment_5/Exercise 2/src/Customer.java
+++ SoftCon2020_Assignment_5/Exercise 2/src/Customer.java
@@ -4,11 +4,18 @@
 public class Customer {
     private String cartype;
     public String getCartype() {return cartype; }
-
+    
+    
+    /**
+     * Customers have a specific car type and a method to print the car type's name, space, speed and price
+     */
     public Customer(String cartype) {
         this.cartype = cartype;
     }
 
+     /**
+     * Prints the name, space, speed and price of the specific car type
+     */ 
     public void ride(){
         System.out.println(cartype.getName() + "; "
                 + cartype.getSpace() + "; "
