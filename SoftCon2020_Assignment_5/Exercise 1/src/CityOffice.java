da674aca5e7fd69ebc4d1d1e6535c8c4bfbae161
Tony Bang <71351936+tonybvnq@users.noreply.github.com>
2020-11-19 15:56:57 +0100

    Update CityOffice.java

--- SoftCon2020_Assignment_5/Exercise 1/src/CityOffice.java
+++ SoftCon2020_Assignment_5/Exercise 1/src/CityOffice.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+* An office in a specific city with all its bakeries
+*/
 public class CityOffice {
     private String name;
     private List<Bakery> childBakeries;
@@ -13,14 +16,24 @@ public class CityOffice {
         this.childBakeries = new ArrayList<>();
     }
 
+    /**
+    * Adds bakery to the city
+    */
     public void addBakery(Bakery bakery) {
         childBakeries.add(bakery);
     }
 
+    /**
+    * removes bakery from the city
+    */
     public void removeBakery(Bakery bakery) {
         childBakeries.remove(bakery);
     }
 
+    
+    /**
+    * prints all bakeries from a specific city 
+    */
     public void printAllBakeriesNames() {
         System.out.print(getChildBakeries());
     }
