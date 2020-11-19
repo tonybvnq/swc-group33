30dbebe19770220ae857b680e6eb8d10ee96ba7a
Tony Bang <71351936+tonybvnq@users.noreply.github.com>
2020-11-19 15:52:33 +0100

    Update CentralOffice.java

--- SoftCon2020_Assignment_5/Exercise 1/src/CentralOffice.java
+++ SoftCon2020_Assignment_5/Exercise 1/src/CentralOffice.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * CentralOffice has a list of all city officeces and prints them with all the bakeries
+ */
 public class CentralOffice {
     private List<CityOffice> allOffices;
     public List<CityOffice> getAllOffices() {return allOffices;}
@@ -9,15 +12,24 @@ public class CentralOffice {
     public CentralOffice() {
         this.allOffices = new ArrayList<>();
     }
-
+    
+    /**
+    * adds an office to the list
+    */
     public void addOffice(CityOffice office) {
         allOffices.add(office);
     }
-
+    
+    /**
+    * removes an office to the list
+    */
     public void removeOffice(CityOffice office) {
         allOffices.remove(office);
     }
 
+    /**
+    * prints all city offices with their bakeries
+    */
     public void printAllOffices() {
         for (CityOffice cityoffice : allOffices) {
             cityoffice.printAllBakeriesNames();
