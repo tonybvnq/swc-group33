1e27b4c8adca62d3843904f125a307cba9751b50
eoeaee <47557704+eoeaee@users.noreply.github.com>
2020-11-19 16:27:58 +0100

    Architecture Added

---
+++ SoftCon2020_Assignment_5/Exercise 2/src/CheapPrice.java
@@ -0,0 +1,3 @@
+public class CheapPrice implements Price {
+
+}
--- SoftCon2020_Assignment_5/Exercise 2/src/Vehicle.java
+++ SoftCon2020_Assignment_5/Exercise 2/src/Vehicle.java
@@ -1,3 +1,21 @@
 abstract class Vehicle {
+    Price price;
+    Propulsion propulsion;
+    Room room;
+    Speed speed;
+
+    public void setPrice(Price price){
+        this.price = price;
+    }
+
+
+}
+
+class MicroCar extends Vehicle{
+    public MicroCar(){
+        this.setPrice(CheapPrice);
+    }
+
+
 
 }
