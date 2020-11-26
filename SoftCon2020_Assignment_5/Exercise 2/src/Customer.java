786de0ed262bb2064bed7f1520dd6833e7101b52
Tony Bang <71351936+tonybvnq@users.noreply.github.com>
2020-11-26 14:34:43 +0100

    Update Customer.java

--- SoftCon2020_Assignment_5/Exercise 2/src/Customer.java
+++ SoftCon2020_Assignment_5/Exercise 2/src/Customer.java
@@ -2,25 +2,33 @@
  * Customers have a specific car type and a method to print the car type's name, space, speed and price
  */
 public class Customer {
-    private String cartype;
-    public String getCartype() {return cartype; }
+    private String vehicletype;
+    public String getCartype() {return vehicletype; }
+    Vehicle vehicle;
     
     
     /**
      * Customers have a specific car type and a method to print the car type's name, space, speed and price
      */
-    public Customer(String cartype) {
-        this.cartype = cartype;
+    public Customer(String vehicletype) {
+        this.vehicletype = vehicletype;
+        switch (vehicletype) {
+            case "Bus" -> vehicle = new Bus();
+            case "FamilyCar" -> vehicle = new FamilyCar();
+            case "MicroCar" -> vehicle = new MicroCar();
+            case "Supercar" -> vehicle = new Supercar();
+        }
+
     }
 
      /**
      * Prints the name, space, speed and price of the specific car type
      */ 
     public void ride(){
-        System.out.println(cartype.getName() + "; "
-                + cartype.getSpace() + "; "
-                + cartype.getSpeed() + "; "
-                + cartype.getPrice());
+        System.out.println(vehicletype + "; "
+                + vehicle.getRoom() + "; "
+                + vehicle.getSpeed() + "; "
+                + vehicle.getPrice());
     }
 
 }
