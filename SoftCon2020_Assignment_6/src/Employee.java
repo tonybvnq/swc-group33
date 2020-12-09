0d101978dbdf5bca4176ad217c26367fef49acea
eoeaee <47557704+eoeaee@users.noreply.github.com>
2020-12-09 14:02:30 +0100

    Added Model Class Employee

---
+++ SoftCon2020_Assignment_6/src/Employee.java
@@ -0,0 +1,43 @@
+/**
+ * Model Class
+ * name, a surname, an address, a telephone number and a unique ID number (a sequence of 8 characters or numbers)
+ */
+
+public class Employee {
+    String name;
+    String surname;
+    String address;
+    String telephoneNumber;
+    String uniqueID;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+    public String getUniqueID() {
+        return uniqueID;
+    }
+    public void setUniqueID(String uniqueID) {
+        this.uniqueID = uniqueID;
+    }
+}
---
+++ SoftCon2020_Assignment_6/src/MVCPatternDemo.java
@@ -0,0 +1,19 @@
+public class MVCPatternDemo {
+    public static void main(String[] args)  {
+
+
+
+
+        Employee employee1 = new Employee();
+
+        employee1.setName("John");
+        employee1.setSurname("Doe");
+        employee1.setAddress("Test Street 123");
+        employee1.setTelephoneNumber("+41 41 000 00 00");
+        employee1.setUniqueID("12345678");
+
+
+
+
+    }
+}
